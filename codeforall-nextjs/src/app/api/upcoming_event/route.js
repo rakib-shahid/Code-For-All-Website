aaa7f1e900c9212c8835a75dc4b65a199902a2ad
rakib-shahid <rakibshahid1298@gmail.com>
2025-01-25 20:07:17 -0500

    added endpoints for events

---
+++ codeforall-nextjs/src/app/api/upcoming_event/route.js
@@ -0,0 +1,29 @@
+import { Pool } from "pg";
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+});
+
+export async function GET() {
+  try {
+    const client = await pool.connect();
+    const result = await client.query(
+      "SELECT * FROM events WHERE event_time > NOW() ORDER BY event_time ASC LIMIT 1;"
+    );
+    client.release();
+
+    return new Response(JSON.stringify(result.rows), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Database error:", error);
+    return new Response(
+      JSON.stringify({ error: "Database connection failed" }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+}
