3296cc744e48f27dd5c8e539885d966284e0d2ad
rakib-shahid <rakibshahid1298@gmail.com>
2025-02-01 01:30:58 -0500

    added speed and analytics

--- codeforall-nextjs/src/app/layout.js
+++ codeforall-nextjs/src/app/layout.js
@@ -1,5 +1,7 @@
 import "./globals.css";
 import { Providers } from "./providers";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = {
   title: "Code For All",
@@ -11,7 +13,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+          <SpeedInsights />
+          <Analytics />
+        </Providers>
       </body>
     </html>
   );
